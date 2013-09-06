3edb9cc6f2154610181014f3b00a40617664552b
ninety47 <mjwok@ninety47.com>
2013-09-06 11:02:22 +1000

    Updated license information in the source code.

--- src/dukascopy.cpp
+++ src/dukascopy.cpp
@@ -1,6 +1,21 @@
-/**
- * Copyright 2013 Michael O'Keeffe.
- */
+/*
+Copyright 2013 Michael O'Keeffe (a.k.a. ninety47).
+
+This file is part of ninety47 Dukascopy toolbox.
+
+The "ninety47 Dukascopy toolbox" is free software: you can redistribute it
+and/or modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation, either version 3 of the License,
+or any later version.
+
+"ninety47 Dukascopy toolbox" is distributed in the hope that it will be
+useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+"ninety47 Dukascopy toolbox".  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include "ninety47/dukascopy.h"
 #include <boost/date_time/posix_time/posix_time.hpp>
